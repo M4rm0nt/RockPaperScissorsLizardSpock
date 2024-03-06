87ba146297c712c857e075da5bdcf98d75c29b61
marmont <mar.mont@example.com>
2024-03-06 10:00:19 +0100

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 public class Main {
     private static final Random random = new Random();
+    private static int siege = 0;
+    private static int niederlagen = 0;
+    private static int unentschieden = 0;
 
     private enum Auswahl {
         STEIN, PAPIER, SCHERE, ECHSE, SPOCK
     }
 
-    private static final Map<Integer, Auswahl> auswahlMap = Map.of(
+    private static final Map<Integer, Auswahl> AUSWAHLMAP = Map.of(
             1, Auswahl.STEIN,
             2, Auswahl.PAPIER,
             3, Auswahl.SCHERE,
@@ -15,7 +18,7 @@ public class Main {
             5, Auswahl.SPOCK
     );
 
-    private static final Map<Auswahl, Set<Auswahl>> gewinnBedigungen = new EnumMap<>(Map.of(
+    private static final Map<Auswahl, Set<Auswahl>> GEWINNBEDIGUNGEN = new EnumMap<>(Map.of(
             Auswahl.STEIN, EnumSet.of(Auswahl.SCHERE, Auswahl.ECHSE),
             Auswahl.PAPIER, EnumSet.of(Auswahl.STEIN, Auswahl.SPOCK),
             Auswahl.SCHERE, EnumSet.of(Auswahl.PAPIER, Auswahl.ECHSE),
@@ -33,7 +36,9 @@ public class Main {
                 int benutzerWahl = pruefeInput(scanner);
 
                 if (benutzerWahl == 6) {
+                    System.out.println("\n");
                     System.out.println("Spiel beendet.");
+                    zeigeErgebnisse();
                     break;
                 }
 
@@ -59,10 +64,11 @@ public class Main {
     }
 
     private static void playGame(int benutzerWahl) {
-        Auswahl benutzerAuswahl = auswahlMap.get(benutzerWahl);
-        Auswahl computerAuswahl = auswahlMap.get(random.nextInt(5) + 1);
+        Auswahl benutzerAuswahl = AUSWAHLMAP.get(benutzerWahl);
+        Auswahl computerAuswahl = AUSWAHLMAP.get(random.nextInt(5) + 1);
 
-        System.out.println("\nDeine Wahl: " + benutzerAuswahl);
+        System.out.println("\n");
+        System.out.println("Deine Wahl: " + benutzerAuswahl);
         System.out.println("Computer wählte: " + computerAuswahl);
 
         ermittelGewinner(benutzerAuswahl, computerAuswahl);
@@ -71,10 +77,22 @@ public class Main {
     private static void ermittelGewinner(Auswahl benutzerAuswahl, Auswahl computerAuswahl) {
         if (benutzerAuswahl == computerAuswahl) {
             System.out.println("Unentschieden!");
-        } else if (gewinnBedigungen.get(benutzerAuswahl).contains(computerAuswahl)) {
+            unentschieden++;
+        } else if (GEWINNBEDIGUNGEN.get(benutzerAuswahl).contains(computerAuswahl)) {
             System.out.println("Du gewinnst! " + benutzerAuswahl + " schlägt " + computerAuswahl + "!");
+            siege++;
         } else {
             System.out.println("Du verlierst! " + computerAuswahl + " schlägt " + benutzerAuswahl + "!");
+            niederlagen++;
         }
+        System.out.println("\n");
+    }
+
+    private static void zeigeErgebnisse() {
+        System.out.println("\n");
+        System.out.println("Spiel Zusammenfassung:");
+        System.out.println("Siege: " + siege);
+        System.out.println("Niederlagen: " + niederlagen);
+        System.out.println("Unentschieden: " + unentschieden);
     }
 }
